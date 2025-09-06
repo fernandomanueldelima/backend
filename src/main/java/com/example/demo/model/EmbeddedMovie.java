ba60af6589e526453b392801f7c1cf1561796cd7
Fernando de Lima <51708466+fernandomanueldelima@users.noreply.github.com>
2025-09-06 11:10:16 -0300

    Now all the models are completed withing their fields

--- src/main/java/com/example/demo/model/EmbeddedMovie.java
+++ src/main/java/com/example/demo/model/EmbeddedMovie.java
@@ -1,29 +1,13 @@
 package com.example.demo.model;
 
+import com.example.demo.dto.MovieDto;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.List;
-
 @Document(collection = "embedded_movies")
-public class EmbeddedMovie {
+public class EmbeddedMovie extends MovieDto {
     @Id
     private String id;
-    private String title;
-    private String plot;
-    private List<String> genres;
-    private List<String> cast;
-    private Integer year;
-
-    public EmbeddedMovie() {}
-
-    public EmbeddedMovie(String title, String plot, List<String> genres, List<String> cast, Integer year) {
-        this.title = title;
-        this.plot = plot;
-        this.genres = genres;
-        this.cast = cast;
-        this.year = year;
-    }
 
     public String getId() {
         return id;
@@ -31,45 +15,5 @@ public class EmbeddedMovie {
 
     public void setId(String id) {
         this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getPlot() {
-        return plot;
-    }
-
-    public void setPlot(String plot) {
-        this.plot = plot;
-    }
-
-    public List<String> getGenres() {
-        return genres;
-    }
-
-    public void setGenres(List<String> genres) {
-        this.genres = genres;
-    }
-
-    public List<String> getCast() {
-        return cast;
-    }
-
-    public void setCast(List<String> cast) {
-        this.cast = cast;
-    }
-
-    public Integer getYear() {
-        return year;
-    }
-
-    public void setYear(Integer year) {
-        this.year = year;
     }
 }
--- src/main/java/com/example/demo/model/Theater.java
+++ src/main/java/com/example/demo/model/Theater.java
@@ -3,13 +3,95 @@ package com.example.demo.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Document(collection = "theaters")
 public class Theater {
     @Id
     private String id;
-    private String theaterId;
-    private String city;
-    private String state;
+    private Integer theaterId;
+    private Location location;
+
+    public static class Location {
+        private Address address;
+        private Geo geo;
+
+        public static class Address {
+            private String street1;
+            private String city;
+            private String state;
+            private String zipcode;
+
+            public String getStreet1() {
+                return street1;
+            }
+
+            public void setStreet1(String street1) {
+                this.street1 = street1;
+            }
+
+            public String getCity() {
+                return city;
+            }
+
+            public void setCity(String city) {
+                this.city = city;
+            }
+
+            public String getState() {
+                return state;
+            }
+
+            public void setState(String state) {
+                this.state = state;
+            }
+
+            public String getZipcode() {
+                return zipcode;
+            }
+
+            public void setZipcode(String zipcode) {
+                this.zipcode = zipcode;
+            }
+        }
+
+        public static class Geo {
+            private String type;
+            private List<Double> coordinates;
+
+            public String getType() {
+                return type;
+            }
+
+            public void setType(String type) {
+                this.type = type;
+            }
+
+            public List<Double> getCoordinates() {
+                return coordinates;
+            }
+
+            public void setCoordinates(List<Double> coordinates) {
+                this.coordinates = coordinates;
+            }
+        }
+
+        public Address getAddress() {
+            return address;
+        }
+
+        public void setAddress(Address address) {
+            this.address = address;
+        }
+
+        public Geo getGeo() {
+            return geo;
+        }
+
+        public void setGeo(Geo geo) {
+            this.geo = geo;
+        }
+    }
 
     public String getId() {
         return id;
@@ -19,29 +101,19 @@ public class Theater {
         this.id = id;
     }
 
-    public String getTheaterId() {
+    public Integer getTheaterId() {
         return theaterId;
     }
 
-    public void setTheaterId(String theaterId) {
+    public void setTheaterId(Integer theaterId) {
         this.theaterId = theaterId;
     }
 
-    public String getCity() {
-        return city;
+    public Location getLocation() {
+        return location;
     }
 
-    public void setCity(String city) {
-        this.city = city;
+    public void setLocation(Location location) {
+        this.location = location;
     }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    // getters y setters
 }
